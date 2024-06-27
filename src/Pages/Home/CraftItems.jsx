52b7d54f8c33ef6c72cd26e62786df7b5132e076
Siam-al-islam <siamalislam692@gmail.com>
2024-06-27 22:01:50 +0600

    removed code

--- src/Pages/Home/CraftItems.jsx
+++ src/Pages/Home/CraftItems.jsx
@@ -1,6 +1,3 @@
-import { useLoaderData } from "react-router-dom";
-import CraftCard from "./CraftCard";
-import loadedCrafts from "https://filmy-server-bf1m95pxc-siam-al-islams-projects.vercel.app/craft";
 
 const CraftItems = () => {
 
@@ -13,11 +10,11 @@ const CraftItems = () => {
                 <p className="mt-3">Drawing and painting craft items involve creativity, color coordination, and artistic skills to transform simple objects into beautiful art.</p>
             </div>
             <div className="mt-10 grid gap-5 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 px-3">
-                {
+                {/* {
                     loadedCrafts.map(craft => <CraftCard
                         key={craft._id}
                         craft={craft} />)
-                }
+                } */}
             </div>
         </div>
     );
